9a311618452875b2f58bb61bf339ace01602c78d
GM <102281241+Glae19@users.noreply.github.com>
2024-09-04 00:37:34 +0800

    Update config.py

--- tg_bot/modules/config.py
+++ tg_bot/modules/config.py
@@ -59,7 +59,8 @@ class Development(Config):
     SUPPORT_USERS = [5344116851]
     WHITELIST_USERS = [5344116851]
     DONATION_LINK = "@its_megm"
-    CERT_PATH = "C:\\Users\\user\\Downloads\\isrgrootx1.pem"
+    CERT_PATH = r"C:\Users\user\Downloads\isrgrootx1.pem"
+
 
     PORT = 7016
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
@@ -95,7 +96,8 @@ class Development:
     SUPPORT_USERS = 5344116851
     WHITELIST_USERS = 5344116851
     DONATION_LINK ="@its_megm"
-    CERT_PATH = "C:\\Users\\user\\Downloads\\isrgrootx1.pem"
+    CERT_PATH = r"C:\Users\user\Downloads\isrgrootx1.pem"
+
 
     PORT = 7016
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
